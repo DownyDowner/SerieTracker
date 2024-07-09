f1a26f595337be577b44fbaea9beec773c6c4e01
Boris Van Cauter <boris.vancauter@student.hers.be>
2024-07-09 13:28:41 +0200

    Remove unnecessary items and refactor main.ts

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,14 +1,17 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+
+// Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+// Components
+import App from './App.vue'
+
 const vuetify = createVuetify({
-    components,
-    directives,
+  components,
+  directives,
 })
-  
+
 createApp(App).use(vuetify).mount('#app')
