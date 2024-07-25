f895359864f56e824cbb044493de34ef7d688980
Boris Van Cauter <boris.vancauter@student.hers.be>
2024-07-25 09:52:27 +0200

    Add(HomeSerie) Display episode

--- front/src/views/home/HomeSerie.vue
+++ front/src/views/home/HomeSerie.vue
@@ -1,15 +1,48 @@
 <template>
-  <div>{{ serieStore.serie?.nom }}</div>
+  <h1 class="ml-3">{{ nomSerie }}</h1>
+  <v-expansion-panels>
+    <v-expansion-panel
+      v-for="(episodes, saison) in groupedEpisodes"
+      :key="saison"
+    >
+      <v-expansion-panel-title>Saison {{ saison }}</v-expansion-panel-title>
+      <v-expansion-panel-text>
+        <v-list>
+          <v-list-item
+            v-for="episode in episodes"
+            :key="episode.id"
+            :title="`Épisode ${episode.episode}`"
+            :subtitle="episode.nom || 'Aucun'"
+          ></v-list-item>
+        </v-list>
+      </v-expansion-panel-text>
+    </v-expansion-panel>
+  </v-expansion-panels>
 </template>
+
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { onMounted, Ref, ref, computed } from "vue";
 import { useSerieStore } from "../../stores/serie";
 import router from "../../router";
+import { Episode } from "../../models/Episode";
 
 const serieStore = useSerieStore();
+
+const nomSerie = ref("");
+const episodes: Ref<Episode[]> = ref([]);
+
+const groupedEpisodes = computed(() => {
+  return episodes.value.reduce((acc: Record<number, Episode[]>, episode) => {
+    (acc[episode.saison] = acc[episode.saison] || []).push(episode);
+    return acc;
+  }, {});
+});
 
 onMounted(async () => {
   const idSerie = router.currentRoute.value.params.id as string;
   await serieStore.getSerieById(Number(idSerie));
+  if (!serieStore.serie) return;
+  nomSerie.value = serieStore.serie?.nom;
+  episodes.value = serieStore.serie.episodes;
 });
 </script>
