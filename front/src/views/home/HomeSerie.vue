ffcefbd849d08cc4a8e13f2850548c7b08a6b917
Boris Van Cauter <boris.vancauter@student.hers.be>
2024-07-25 11:16:22 +0200

    Feat: Add functionality to add a new episode to the episode list

--- front/src/views/home/HomeSerie.vue
+++ front/src/views/home/HomeSerie.vue
@@ -1,6 +1,25 @@
 <template>
-  <h1 class="ml-3">{{ nomSerie }}</h1>
-  <v-expansion-panels>
+  <v-row class="my-2" align="center" justify="space-between">
+    <v-col cols="6">
+      <v-text-field
+        class="ml-3"
+        v-model="nomSerie"
+        label="Nom de la série"
+        hide-details
+      />
+    </v-col>
+    <v-col cols="6 d-flex justify-end">
+      <v-btn
+        class="mr-3"
+        prepend-icon="mdi-plus-circle"
+        color="warning"
+        @click.stop="addEpisode"
+      >
+        Ajouter un épisode
+      </v-btn>
+    </v-col>
+  </v-row>
+  <v-expansion-panels variant="accordion">
     <v-expansion-panel
       v-for="(episodes, saison) in groupedEpisodes"
       :key="saison"
@@ -13,23 +32,35 @@
             :key="episode.id"
             :title="`Épisode ${episode.episode}`"
             :subtitle="episode.nom || 'Aucun'"
-          ></v-list-item>
+          />
         </v-list>
       </v-expansion-panel-text>
     </v-expansion-panel>
   </v-expansion-panels>
+  <v-row no-gutters class="mt-2">
+    <v-col class="d-flex justify-end align-center">
+      <v-btn class="ma-2" color="grey" @click.stop="close">Retour</v-btn>
+      <v-btn color="success">Sauvegarder</v-btn>
+    </v-col>
+  </v-row>
+  <EditEpisodeDialog ref="editEpisodeDialog" />
 </template>
 
 <script setup lang="ts">
-import { onMounted, Ref, ref, computed } from "vue";
+import { onMounted, ref, computed } from "vue";
 import { useSerieStore } from "../../stores/serie";
 import router from "../../router";
 import { Episode } from "../../models/Episode";
+import { NavigationConst } from "../../router/routeConst";
+import EditEpisodeDialog from "./components/EditEpisodeDialog.vue";
 
 const serieStore = useSerieStore();
 
 const nomSerie = ref("");
-const episodes: Ref<Episode[]> = ref([]);
+const episodes = ref<Episode[]>([]);
+const editEpisodeDialog = ref<InstanceType<typeof EditEpisodeDialog> | null>(
+  null
+);
 
 const groupedEpisodes = computed(() => {
   return episodes.value.reduce((acc: Record<number, Episode[]>, episode) => {
@@ -45,4 +76,14 @@ onMounted(async () => {
   nomSerie.value = serieStore.serie?.nom;
   episodes.value = serieStore.serie.episodes;
 });
+
+const close = () => {
+  serieStore.serie = null;
+  router.push({ name: NavigationConst.nameHome });
+};
+
+const addEpisode = async () => {
+  const episodeToAdd = await editEpisodeDialog.value?.openNew();
+  if (episodeToAdd) episodes.value.push(episodeToAdd);
+};
 </script>
---
+++ front/src/views/home/components/EditEpisodeDialog.vue
@@ -0,0 +1,122 @@
+<template>
+  <v-dialog
+    v-model="isOpen"
+    no-click-animation
+    scroll-strategy="close"
+    max-width="650"
+    max-height="650"
+    persistent
+  >
+    <v-form v-model="isValid" @submit.prevent>
+      <v-card min-height="200">
+        <v-toolbar flat dark color="primary">
+          <v-toolbar-title v-if="isNew">Ajout d'un épisode</v-toolbar-title>
+          <v-toolbar-title v-else>Modifier l'épisode</v-toolbar-title>
+          <v-spacer />
+          <v-btn icon dark @click.stop="close">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-toolbar>
+        <v-card-text>
+          <v-row no-gutters>
+            <v-col cols="12" md="6" class="mb-2">
+              <v-text-field
+                v-model="saison"
+                label="Saison"
+                type="number"
+                :rules="[
+                  (v) => !!v || 'La saison est requise',
+                  (v) => v > 0 || 'La saison doit être un nombre supérieur à 0',
+                ]"
+              />
+            </v-col>
+            <v-col cols="12" md="6" class="mb-2">
+              <v-text-field
+                v-model="episode"
+                label="Épisode"
+                type="number"
+                :rules="[
+                  (v) => !!v || 'L\'épisode est requis',
+                  (v) =>
+                    v > 0 || 'L\'épisode doit être un nombre supérieur à 0',
+                ]"
+              />
+            </v-col>
+          </v-row>
+          <v-text-field
+            v-model="nom"
+            label="Nom de l'épisode (Facultatif)"
+            hide-details
+          />
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            class="ma-2 px-4"
+            color="primary"
+            variant="flat"
+            size="large"
+            @click.stop="close"
+          >
+            Annuler
+          </v-btn>
+          <v-btn
+            type="submit"
+            class="ma-2 px-4"
+            color="success"
+            variant="flat"
+            size="large"
+            :disabled="!isValid"
+            :loading="isLoading"
+            @click.stop="save"
+          >
+            Sauvegarder
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-form>
+  </v-dialog>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { Episode } from "../../../models/Episode";
+
+const isOpen = ref(false);
+const isValid = ref(false);
+const isLoading = ref(false);
+const isNew = ref(false);
+const saison = ref(0);
+const episode = ref(0);
+const nom = ref<string | null>(null);
+
+let resolve: (value: Episode | null) => void;
+
+const openNew = () => {
+  isNew.value = true;
+  isOpen.value = true;
+  return new Promise<Episode | null>(function (res) {
+    resolve = res;
+  });
+};
+
+const close = () => {
+  isOpen.value = false;
+  saison.value = 0;
+  episode.value = 0;
+  nom.value = null;
+  resolve(null);
+};
+
+const save = () => {
+  const episodeToEdit = new Episode();
+  episodeToEdit.saison = saison.value;
+  episodeToEdit.episode = episode.value;
+  episodeToEdit.nom = nom.value;
+  resolve(episodeToEdit);
+  close();
+};
+
+defineExpose({
+  openNew,
+});
+</script>
