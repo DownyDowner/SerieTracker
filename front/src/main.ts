cc7dc9d7f17cb52bd8664faa96495c154da3d169
Boris Van Cauter <boris.vancauter@student.hers.be>
2024-07-27 09:11:19 +0200

    Add vue3-toastify to the project

--- front/src/main.ts
+++ front/src/main.ts
@@ -5,7 +5,15 @@ import vuetify from './plugins/vuetify'
 
 // Components
 import App from './App.vue'
+import Vue3Toastify, { type ToastContainerOptions } from 'vue3-toastify';
 
 const pinia = createPinia()
 
-createApp(App).use(vuetify).use(pinia).use(router).mount('#app')
+createApp(App)
+    .use(vuetify)
+    .use(pinia)
+    .use(router)
+    .use(Vue3Toastify, {
+        autoClose: 3000,
+    } as ToastContainerOptions)
+    .mount('#app')
