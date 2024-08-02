ac88c84fdb37004f476c157e375c18bfeaf3b57b
Boris Van Cauter <boris.vancauter@student.hers.be>
2024-08-02 08:32:03 +0200

    Feat: admin interface with action to export series and episodes to CSV

--- api/projet01/serieTracker/admin.py
+++ api/projet01/serieTracker/admin.py
@@ -1,10 +1,29 @@
+import csv
+
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.http import HttpResponse
 
 from .models import Utilisateur, Episode, Serie, Suivi, Vu
 
 
 # Register your models here.
+
+@admin.action(description='Export selected series and their episodes to CSV')
+def export_series_episodes_to_csv(modeladmin, request, queryset):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="series_episodes.csv"'
+    writer = csv.writer(response)
+
+    writer.writerow(['Serie', 'Saison', 'Numéro', 'Nom'])
+
+    for serie in queryset:
+        episodes = serie.episodes.all()
+        for episode in episodes:
+            writer.writerow([serie.nom, episode.saison, episode.episode, episode.nom])
+
+    return response
+
 
 class EpisodeInline(admin.TabularInline):
     model = Episode
@@ -13,6 +32,7 @@ class EpisodeInline(admin.TabularInline):
 
 class SerieAdmin(admin.ModelAdmin):
     inlines = [EpisodeInline]
+    actions = [export_series_episodes_to_csv]
 
 
 admin.site.register(Utilisateur, UserAdmin)
