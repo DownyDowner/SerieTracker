9f266df211e0d7f52b746270a330d834d47d3857
Boris Van Cauter <boris.vancauter@student.hers.be>
2024-08-09 09:49:58 +0200

    Chore: change database configuration to PostgreSQL

--- api/projet01/projet01/settings.py
+++ api/projet01/projet01/settings.py
@@ -88,9 +88,14 @@ WSGI_APPLICATION = 'projet01.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "admin",
+        "HOST": "localhost",
+        "PORT": "5432",
+        "ATOMIC_REQUESTS": True,
     }
 }
 
