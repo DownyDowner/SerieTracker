2e52d95c802587b13ddb17343af46a960c928576
Boris Van Cauter <boris.vancauter@student.hers.be>
2024-08-10 20:16:44 +0200

    Chore: adding docker-compose.yml for Docker deployment

--- api/projet01/projet01/settings.py
+++ api/projet01/projet01/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-$&@*=#1p(q_)ue!lc13sf^)zt7x#r*@nta7ux_2gdowv#txo_v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "back"]
 
 AUTH_USER_MODEL = 'serieTracker.Utilisateur'
 
@@ -93,7 +93,7 @@ DATABASES = {
         "NAME": "postgres",
         "USER": "postgres",
         "PASSWORD": "admin",
-        "HOST": "localhost",
+        "HOST": "db",
         "PORT": "5432",
         "ATOMIC_REQUESTS": True,
     }
@@ -132,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
